6c04448145c05527ea711e5ed70e8f4d2bc0f9d3
ersanjaysah <100331991+ersanjaysah@users.noreply.github.com>
2024-01-01 15:14:21 +0530

    Update securityGP.tf

--- securityGP.tf
+++ securityGP.tf
@@ -1,22 +1,22 @@
+variable "ports" {
+  type = list(number)
+  default = [ 22, 80, 443 ]
+}
+
 resource "aws_security_group" "nginx_SG" {
   name        = "nginx-SG"
   description = "this sg of nginx by tf"
 
-  ingress {
-    description = "allow port 22"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+dynamic "ingress" {
+  for_each = var.ports
+  content {
+    description = "inbound rule"
+    from_port = ingress.value
+    to_port = ingress.value
+    protocol = "tcp"
+    cidr_blocks = [ "0.0.0.0/0" ]
   }
-
-  ingress {
-    description = "allow port 80"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+}
 
   egress {
     from_port   = 0
@@ -26,7 +26,7 @@ resource "aws_security_group" "nginx_SG" {
   }
 
   tags = {
-    Name = "nginx-sg"
+    Name = "sg-looping"
   }
 }
 
